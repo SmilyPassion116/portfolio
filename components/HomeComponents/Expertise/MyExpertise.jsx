24a981cba86781817c659f9716be47599214cbfc
betpractice116 <bestpractice8116@gmail.com>
2025-03-01 20:18:38 -0800

    fix: add margin

--- components/HomeComponents/Expertise/MyExpertise.jsx
+++ components/HomeComponents/Expertise/MyExpertise.jsx
@@ -16,7 +16,7 @@ const MyExpertise = () => {
     <>
       {isNonEmptyArray(data) && (
         <>
-          <div className="grid justify items-center grid-flow-row  grid-rows-auto gap-4 ">
+          <div className="grid justify items-center grid-flow-row  grid-rows-auto gap-4 mt-8">
             {isLoading
               ? [1, 2, 3, 4, 5, 6].map(() => (
                   <ParagraphSkeleton className={'space-y-2 p-8'} />
--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -2,7 +2,7 @@ const expertise = [
   {
     id: 0,
     title: 'About MySelf',
-    desc: 'Engineered high-performance Web3 applications using React, Next.js, ethers.js, and wagmi, ensuring seamless interaction with blockchain networks while maintaining an average loading time under 2 seconds. Prioritized user experience by implementing responsive design principles, enhancing accessibility for diverse users across decentralized platforms. I am really eager to develop my skills and learn new skills'
+    desc: 'Engineered high-performance Web3 applications using React, Next.js, ethers.js, and wagmi, ensuring seamless interaction with blockchain networks while maintaining an average loading time under 2 seconds. Prioritized user experience by implementing responsive design principles, enhancing accessibility for diverse users across decentralized platforms. I am really eager to develop my skills and learn new skills.'
   }
 ];
 
