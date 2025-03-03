d725c593f6fc470b02d4982ceafbd461b87ac90a
betpractice116 <bestpractice8116@gmail.com>
2025-03-03 03:57:15 -0800

    fix: add loading status

--- components/HomeComponents/Expertise/MyExpertise.jsx
+++ components/HomeComponents/Expertise/MyExpertise.jsx
@@ -14,19 +14,11 @@ const MyExpertise = () => {
 
   return (
     <>
-      {isNonEmptyArray(data) && (
-        <>
-          <div className="grid justify items-center grid-flow-row  grid-rows-auto gap-4 mt-8">
-            {isLoading
-              ? [1, 2, 3, 4, 5, 6].map(() => (
-                  <ParagraphSkeleton className={'space-y-2 p-8'} />
-                ))
-              : data?.map((data, key) => (
-                  <ExpertiseCard key={key} data={data} />
-                ))}
-          </div>
-        </>
-      )}
+      <div className="grid justify items-center grid-flow-row  grid-rows-auto gap-4 mt-8">
+        {isLoading
+          ? [1, 2].map(() => <ParagraphSkeleton className={'space-y-2 p-8'} />)
+          : data?.map((data, key) => <ExpertiseCard key={key} data={data} />)}
+      </div>
     </>
   );
 };
